6749c359cc1be5c800ff1f71dfda5fc39cf5d2a2
Vamsi Avula <avamsi07@gmail.com>
2023-09-03 04:39:14 +0530

    refactor toggle out into a function

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,43 @@
 import * as vscode from "vscode";
 
+async function toggle(workspaceState: vscode.Memento) {
+  if (workspaceState.get("maximize")) {
+    await vscode.commands.executeCommand("workbench.action.closeSidebar");
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleActivityBarVisibility",
+    );
+    await vscode.commands.executeCommand("workbench.action.closeAuxiliaryBar");
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleStatusbarVisibility",
+    );
+    vscode.window.activeTerminal?.show();
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleMaximizedPanel",
+    );
+    workspaceState.update("maximize", false);
+  } else {
+    vscode.window.activeTerminal?.hide();
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleStatusbarVisibility",
+    );
+    await vscode.commands.executeCommand("workbench.action.toggleAuxiliaryBar");
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleActivityBarVisibility",
+    );
+    await vscode.commands.executeCommand(
+      "workbench.action.toggleSidebarVisibility",
+    );
+    workspaceState.update("maximize", true);
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("tmax.toggle", async () => {
-    if (context.workspaceState.get("maximize")) {
-      await vscode.commands.executeCommand("workbench.action.closeSidebar");
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleActivityBarVisibility",
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.closeAuxiliaryBar",
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleStatusbarVisibility",
-      );
-      vscode.window.activeTerminal?.show();
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleMaximizedPanel",
-      );
-      context.workspaceState.update("maximize", false);
-    } else {
-      vscode.window.activeTerminal?.hide();
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleStatusbarVisibility",
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleAuxiliaryBar",
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleActivityBarVisibility",
-      );
-      await vscode.commands.executeCommand(
-        "workbench.action.toggleSidebarVisibility",
-      );
-      context.workspaceState.update("maximize", true);
-    }
-  });
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "tmax.toggle",
+      async () => await toggle(context.workspaceState),
+    ),
+  );
 }
 
 export function deactivate() {}
